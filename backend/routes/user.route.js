6c6c1ef5c0106a1167b411f2824c48e2b76fa17b
Pratyasha Palit <pratyasha2005palit.com>
2025-05-28 12:52:53 +0530

    add travel story api route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { getUsers } from "../controllers/user.controller.js"
 import { verifyToken } from "../utils/verifyUser.js"
+
 const router = express.Router()
 router.get("/getusers", verifyToken, getUsers)
 
