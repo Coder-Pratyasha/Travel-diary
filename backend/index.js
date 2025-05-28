beb0e9af328f7c20a141c66c502ca827db00badb
Pratyasha Palit <pratyasha2005palit.com>
2025-05-28 17:49:11 +0530

    image uploading api roue

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import travelStoryRoutes from "./routes/travelStory.route.js"
+import path from "path"
+import { fileURLToPath } from "url"
 
 dotenv.config()
 
@@ -32,6 +34,12 @@ app.use("/api/auth", authRoutes)
 
 app.use("/api/user", userRoutes)
 app.use("/api/travel-story", travelStoryRoutes)
+
+ //server static files from the uploads and assests directory
+ const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
 app.use((err,req,res,next) => {
     const statusCode = err.statusCode || 500
