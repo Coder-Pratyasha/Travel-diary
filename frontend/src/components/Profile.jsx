9c6922defdeb1be6218b7c55f6e64eb90a8cb0b9
Pratyasha Palit <pratyasha2005palit.com>
2025-06-03 13:20:32 +0530

    Signout api integration

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from "react-redux"
 import { getInitials } from '../utils/helper'
 
-const Profile = () => {
+const Profile = ({onLogout}) => {
     const { currentUser }=useSelector((state)=>state.user)
   return (
     <div className=" flex items-center gap-3">
@@ -11,7 +11,7 @@ const Profile = () => {
         </div>
       <div>
         <p className="text-lg font-medium">{currentUser.username || ""}</p>
-        <button className="text-sm text-red-600 underline">Logout</button>
+        <button className="text-sm text-red-600 underline" onClick={onLogout}>Logout</button>
       </div>
     </div>
   )
