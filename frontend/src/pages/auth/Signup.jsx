9bfe843742f5916da730c77b7521a27fe2ec5681
Pratyasha Palit <pratyasha2005palit.com>
2025-07-21 17:09:12 +0530

    Changes in UI

--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -80,7 +80,12 @@ const Signup = () => {
    
       <div className="bg-white/80 w-full max-w-sm p-6 md:p-8 rounded-lg shadow-lg z-10">
             <form onSubmit={handleSubmit}>
-              <h4 className="text-2xl font-semibold mb-7">Create Your Account</h4>
+              <h1 className="font-bold text-2xl sm:text-2xl flex flex-col items-center justify-center">
+              <span className="text-blue-600">MILES AND MEMORIES</span>
+              <h4 className="text-xl font-semibold mb-6 text-center md:text-left">
+                Create Your Account
+              </h4>
+              </h1>
 
               <input
                 type="text"
